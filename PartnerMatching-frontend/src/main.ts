90cf06090d755c438c5f6ab451fae7927c5286f7
Hty <15161656368@163.com>
2026-01-16 10:43:44 +0800

    提交文件

--- PartnerMatching-frontend/src/main.ts
+++ PartnerMatching-frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { NavBar } from 'vant';
 
-createApp(App).mount('#app')
+const app = createApp(App).mount('#app')
+app.use(NavBar);
