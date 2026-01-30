08c72b46e537c2000983a385c5019854cb372a4b
Hty <15161656368@163.com>
2026-01-30 15:35:38 +0800

    提交文件

---
+++ PartnerMatching-backend/src/main/java/com/hty/partnermatching/Job/PreCacheJob.java
@@ -0,0 +1,50 @@
+package com.hty.partnermatching.Job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hty.partnermatching.mapper.UserMapper;
+import com.hty.partnermatching.model.domain.User;
+import com.hty.partnermatching.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 缓存预热
+ */
+@Component
+@Slf4j
+public class PreCacheJob {
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    //重点用户
+    private List<Long> mainUserList = Arrays.asList(1L);
+
+    //每天执行，预热推荐用户
+    @Scheduled(cron = "0 4 0 * * *")
+    public void doCacheRecommendUser(){
+        for (Long userId : mainUserList){
+            QueryWrapper queryWrapper = new QueryWrapper<>();
+            Page<User> userPage = userService.page(new Page<>(1,20),queryWrapper);
+            String redisKey = String.format("partnermatching:user:recommend:%s",userId);
+            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+            //写缓存
+            try {
+                valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+            }catch (Exception e){
+                log.error("redis set key error",e);
+            }
+        }
+    }
+}
